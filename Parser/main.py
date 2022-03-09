1a7cab5c1dbc406969734a0e5d7880b5efaf56df
TarGrox <dima-ano0@yandex.ru>
2022-03-09 18:07:56 +0300

    updated files

--- Parser/main.py
+++ Parser/main.py
@@ -4,27 +4,5 @@ import requests
 import json
 import os
 
-state = 'FL'
-headers = {
-    'accept': 'application/geo+json',
-    'User-Agent': 'application in Python; email, if you need: dima-ano0@yandex.ru',
-}
-params = (
-    ('state', state),
-)
 
-response = requests.get('https://api.weather.gov/stations', headers=headers, params=params)
-print(response.status_code)
-
-rewrite_flag = False
-if (response.status_code == 200) and (rewrite_flag == True):
-
-    dirname = 'utils'
-    filename = 'data_stations_file.json'
-    dirpath = os.path.join(os.getcwd(), dirname)
-    filename = os.path.join(dirpath, filename)
-    
-    with open(filename, "w") as write_file:
-        json.dump(response.json(), write_file)
-        print('data_file has been overwritten')
 
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## Hello!
 ### This is Data Engineer project
 
-Theme of this project - is collect data from API of [The National Weather Service (NWS)](https://www.weather.gov/documentation/services-web-api#/)
 
-Here is a description of API:
-> The National Weather Service (NWS) API allows developers access to critical forecasts, alerts, and observations, along with other weather data. 
+Switch from weather.gov to lamoda.ru
