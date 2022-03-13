19e720bbd113fee611b13dd81bd0c1474b6f69a4
TarGrox <dima-ano0@yandex.ru>
2022-03-14 00:28:43 +0300

    rewrote, add classes and bunch of methods

--- Parser/main.py
+++ Parser/main.py
@@ -7,30 +7,50 @@ from lxml import etree
 import requests
 # ==========================================
 
-# https://www.lamoda.ru/c/513/clothes-muzhskie-d-insy/?sitelink=topmenuM&l=7
+# https://www.lamoda.ru/p/rtlabb019901/clothes-hebymango-dzhinsy/
 
-# сделать async/await -> подавать кусками в Парсер через трансформацию в file-like object
-lamoda_link = 'https://www.lamoda.ru'
-r = requests.get('https://www.lamoda.ru/c/513/clothes-muzhskie-d-insy/')
-print(r.status_code)
+class Parser():
+    def __init__(self):
+        #? Ссылку La_link подавать с '/' в конце?
+        #? .collect_links, если что, возвращает ссылки с '/' в начале
+        self.La_link = 'https://www.lamoda.ru'
+        self.xpath_to_collect_links = '//a[@class="x-product-card__link x-product-card__hit-area"]/@href'
+    
+    def collect_links(self, link):
+        page = RequestsClass.get_html(link)
+        links_of_product = page.xpath(self.xpath_to_collect_links)
+        print('success')
+        
+        return links_of_product
+    
+    def extract_inf_from_js(self, link):
+        pass
+    
+    def get_next_link(link):
+        #TODO: определить в каком виде получать и передавать дальше ссылку
+        pass
 
-rewrite_flag = False
-if (r.status_code == 200) and (rewrite_flag == True):
-    namefile = 'myfile.html'
-    f = open(namefile, 'w')
-    f.write(r.text)
-    print(f'file {namefile} has been overwritten')
 
-html_etree = etree.HTML(r.text)
-links_of_product = html_etree.xpath('//a[@class="x-product-card__link x-product-card__hit-area"]/@href')
-
-print(len(links_of_product))
-print(links_of_product)
+class RequestsClass():
+    def get_html(link):
+        r = requests.get(link)
+        #? Подумать над реализацией, если код != 200, что возвращать?
+        print(r.status_code)
+        return etree.HTML(r.text) if r.status_code == 200 else None
+    
+    def collect_reviews(link):
+        pass
 
 # ========================================
 # Обработка страницы продукта:
 
+Pr = Parser()
+Rc = RequestsClass()
+links_of_product = Pr.collect_links('https://www.lamoda.ru/c/517/clothes-muzhskie-bryuki/')
+print(links_of_product)
+
+
 for link in links_of_product:
-    # r = requests.get('https://www.lamoda.ru' + link)
-    print(lamoda_link + link)
+    # page = Rc.get_html(link)
+    print(Pr.La_link + link)
     
--- Parser/parse2.py
+++ Parser/parse2.py
@@ -11,13 +11,11 @@ print(new_path)
 # причем, нужная информация хранится почти в виде dict питоновских.
 # В виде прототипа решил вырезать этот кусок кода (работая с ним, как с текстом), после уже трансформируя в dict
 with open(new_path, 'r') as f:
-    
     st = f.read()
     mark_product = st.find('"product":{')
     mark_comments_data = st.find('"comments_data":{')
     inf_prod = st[mark_product+len('"product":{')-1:mark_comments_data-1]
 
 sts = json.loads(inf_prod)
-print(sts["sizes"])
 
 1+1
